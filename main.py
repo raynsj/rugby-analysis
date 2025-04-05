06971efb9ca550c0b91aba6218a1a834c3614a18
Rayn <108568491+raynsj@users.noreply.github.com>
2025-04-05 14:57:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def main():
     subprocess.check_call(["pip", "install", "opencv-python-headless", "numpy", "scikit-learn"])
     
     # read video
-    video_frames = read_video('input_videos/nrltest_video.mov')
+    video_frames = read_video('input_videos/input_video.mp4')
 
     # Initialize tracker
     tracker = Tracker('modelsv0.0.4/best(4).pt')
 
     # Get object tracks
-    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stub2.pkl')
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stub.pkl')
     
     # Process video to get player metrics
     processed_tracks = tracker.process_video(video_frames, tracks)
@@ -23,7 +23,7 @@ def main():
     output_video_frames = tracker.draw_annotations(video_frames, processed_tracks)
     
     # save video
-    save_video(output_video_frames, 'output_videos/nrl_test_video.mp4')
+    save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 
 if __name__ == "__main__":
