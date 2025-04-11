29a94e092b294daaf35549a94e221ddd2b399e0d
Rayn <108568491+raynsj@users.noreply.github.com>
2025-04-11 15:15:46 +0800

    Update conversion_script.py
    change folder and file names

--- conversion_script.py
+++ conversion_script.py
@@ -3,7 +3,7 @@ import coremltools as ct
 from ultralytics import YOLO
 
 # Load original PyTorch model
-model = YOLO('modelsv0.0.4/best(4).pt')
+model = YOLO('models/model.pt')
 
 # Export to Core ML format
 model.export(format='coreml')  # This creates 'best(4).mlmodel'
